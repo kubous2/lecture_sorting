92f7ea47e954975b06cea0a8bc482fca679bd3a1
Jakub Rehurek <jakub.rehurek@seznam.cz>
2024-04-15 11:53:27 +0200

    read data

--- sorting.py
+++ sorting.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 
@@ -8,12 +9,25 @@ def read_data(file_name):
     :param file_name: (str), name of CSV file
     :return: (dict), dictionary with numeric data, keys - csv column names, values - numbers in each column
     """
+
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open (file_path, "r") as f:
+        reader = csv.DictReader(f)
+        dict = {}
+        for row in reader:
+            for header, value in row.items():
+                if header not in dict:
+                    dict[header] = [int(value)]
+                else:
+                    dict [header].append(int(value))
+    return dict
+
 
 
 def main():
-    pass
+    data = read_data("numbers.csv")
+    print(data)
 
 
 if __name__ == '__main__':
